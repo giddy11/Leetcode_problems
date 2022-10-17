6bcab1aeab045fb740dffadb0010d5aa52955d77
gideon <edoghotugiddy@gmail.com>
2022-10-17 22:55:49 +0100

    done

--- LeetCode_Easy/LeetCode_Easy/Program.cs
+++ LeetCode_Easy/LeetCode_Easy/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+ 
+
+ int RomanToInt(string s)
+{
+    Dictionary<char, int> dict = new Dictionary<char, int> { { 'I', 1 }, { 'V', 5 }, { 'X', 10 }, { 'L', 50 }, { 'C', 100 }, { 'D', 500 }, { 'M', 1000 } };
+
+
+    char[] ch = s.ToCharArray();
+    int result = 0;
+    int intVal, nextIntValue;
+
+    for (int i = 0; i < ch.Length; i++)
+    {
+        intVal = dict[ch[i]];
+
+        if (i != ch.Length-1)
+        {
+            nextIntValue = dict[ch[i + 1]];
+
+            if (nextIntValue>intVal)
+            {
+                intVal = nextIntValue - intVal;
+                i += 1;
+            }
+        }
+        result += intVal;
+    }
+    return result;
+}
+
+Console.WriteLine(RomanToInt("LVIII"));
+
+Console.ReadLine();
