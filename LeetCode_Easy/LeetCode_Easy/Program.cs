66b0e4bc1da576813064a0b47f2d0e991591e645
gideon <edoghotugiddy@gmail.com>
2022-10-21 07:31:16 +0100

    not displaying the result correctly on the console

--- LeetCode_Easy/LeetCode_Easy/Program.cs
+++ LeetCode_Easy/LeetCode_Easy/Program.cs
@@ -24,14 +24,8 @@ int[] nums = new int[] {3,6,4,5,3,4,5,6,7,5,4,4,4,4,4,3,3,2,2,1};
 int target = 11;
 var result = TwoSum(nums, target);
 
-for (int i = 0; i < result.Length; i++)
-{
-    //if (nums[0] + nums[1] == target)
-    //{
-    //    Console.Write($"[{i},{i + 1}]");
-    //}
-    //else Console.WriteLine("There is no array that sums to give the target value.");
+Console.Write($"[{result[0]}, {result[1]}]");
 
-    Console.Write($"[{i},{i + 1}]");
-}
+
+Console.ReadLine();
 
