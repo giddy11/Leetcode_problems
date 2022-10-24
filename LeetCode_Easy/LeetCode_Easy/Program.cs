f2aa741e6a61b216ff1340527576fb05f2c62227
giddy11 <edoghotugiddy@gmail.com>
2022-10-24 09:26:34 +0100

    k

--- LeetCode_Easy/LeetCode_Easy/Program.cs
+++ LeetCode_Easy/LeetCode_Easy/Program.cs
@@ -1,3 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+int ClimbStairs(int n)
+{
+    if (n <= 2)
+    {
+        return n;
+    }
 
+    int prev1 = 1;
+    int prev2 = 2;
+    int current = 0;
+
+    for (int i = 3; i <= n; i++)
+    {
+        current = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = current;
+    }
+    return current;
+    //1 2 3 4 5
+    //1 2 3 5 8
+}
+
+Console.WriteLine(ClimbStairs(5));
