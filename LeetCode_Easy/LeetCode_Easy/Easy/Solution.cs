f7b232cae5751ae5d4c6b3c1dae5dc0d276e7107
Gideon Edoghotu <edoghotugiddy@gmail.com>
2023-11-05 20:32:45 +0100

    solved

---
+++ LeetCode_Easy/LeetCode_Easy/Easy/Solution.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Easy.Easy
+{
+    public class Solution
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int count = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[count++] = nums[i];
+                    //count++;
+                }
+            }
+            return count;
+        }
+
+        public int SearchInsert(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] >= target)
+                    return i;
+            }
+            return nums.Length;
+        }
+    }
+
+    public static class Solution1
+    {
+
+        public static int[] PlusOne(int[] digits)
+        {
+            int len = digits.Length;
+            int final = len - 1;
+
+            for (int i = final; i >= 0; i--)
+            {
+                if (digits[i] < 9)
+                {
+                    digits[i]++;
+                    return digits;
+                }
+                digits[i] = 0;
+            }
+
+            var numberArr = new int[len + 1];
+            numberArr[0] = 1;
+            return numberArr;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //public static int[] PlusOne(int[] digits)
+        //{
+        //    int n = digits.Length;
+        //    for (int i = n - 1; i >= 0; i--)
+        //    {
+        //        if (digits[i] < 9)
+        //        {
+        //            digits[i]++;
+        //            return digits;
+        //        }
+        //        digits[i] = 0;
+        //    }
+        //    int[] newNumber = new int[n + 1];
+        //    newNumber[0] = 1;
+
+        //    return newNumber;
+        //}
+    }
+
+    public class Solution2
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0)
+                return false;
+
+            if (x < 10)
+                return true;
+
+            int digit, rev_x = 0;
+            int orig_x = x;
+
+            while(x > 0)
+            {
+                digit = x % 10;
+                rev_x = rev_x * 10 + digit;
+                x /= 10;
+            }
+
+            if (rev_x == orig_x)
+                return true;
+            else
+                return false;
+        }
+    }
+}
--- LeetCode_Easy/LeetCode_Easy/Program.cs
+++ LeetCode_Easy/LeetCode_Easy/Program.cs
@@ -1,3 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+/*
+using LeetCode_Easy.Easy;
 
+Solution solution = new Solution();
+
+int[] nums = { 3, 2, 2, 3 };
+int[] nums1 = { 1, 2, 5, 6 };
+
+int val = 3;
+int target = -2;
+
+int newLength = solution.RemoveElement(nums, val);
+int newLength1 = solution.SearchInsert(nums1, target);
+
+Console.WriteLine("Modified Array: ");
+
+//for (int i = 0; i < newLength; i++)
+//{
+//    Console.WriteLine($"{nums[i]} ");
+//}
+//Console.WriteLine("\n");
+
+Console.WriteLine($"Number of elements not equal to {val}: {newLength1}\n");
+*/
+
+/*
+using LeetCode_Easy.Easy;
+
+int[] result1 = Solution1.PlusOne(new int[] { 8,9 });
+*/
+
+
+using LeetCode_Easy.Easy;
+
+var test = new Solution2();
+bool x = test.IsPalindrome(-101);
+
+
+
+Console.WriteLine(x);
